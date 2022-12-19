15c816ed114f4f3b497b3b04ac47078752131096
KevenSoares22 <keven.v.soares@gmail.com>
2022-12-18 21:10:02 -0300

    Formulario de Login

--- js/script.js
+++ js/script.js
@@ -1,1 +1,34 @@
+let apresentacao = document.querySelector('.apresentacao')
+let login = document.querySelector('.login')
+let cadastro = document.querySelector('.cadastro')
+let divs = document.querySelectorAll('main div')
+let buttons = document.querySelectorAll('main button.pr')
+let buttonsDiv = document.querySelector('main button.pr').closest('div')
 
+
+buttons.forEach((button) =>{
+    button.addEventListener('click', ()=>{
+        
+        
+        
+        let btnClass = button.closest('div')
+        showContent(btnClass)
+
+
+
+    })
+})
+let showContent = (btnClass) =>{
+
+    console.log(btnClass.className)
+    divs.forEach((div)=>{
+        div.classList.remove('ativo')
+
+
+
+    })
+    
+    login.classList.add('ativo')
+    
+
+}
